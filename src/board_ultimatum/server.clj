b81589b5f2c2284295c3b776c8525a8c6c54593c
Ryan McGowan <ryan@ryanmcg.com>
2012-10-03 19:09:54 -0400

    Connect to the model (mongo) when the server is started.

--- src/board_ultimatum/server.clj
+++ src/board_ultimatum/server.clj
@@ -1,11 +1,19 @@
 (ns board-ultimatum.server
-  (:require [noir.server :as server]))
+  (:require [noir.server :as server]
+            [board-ultimatum.engine.model :as model]
+            [board-ultimatum.engine.config :as config]))
 
 (server/load-views-ns 'board-ultimatum.views)
 
 (defn -main [& m]
   (let [mode (keyword (or (first m) :dev))
         port (Integer. (get (System/getenv) "PORT" "8080"))]
-    (server/start port {:mode mode
-                        :ns 'board-ultimatum})))
-
+    (try
+      (model/connect)
+      (server/start port {:mode mode
+                          :ns 'board-ultimatum})
+      (catch java.io.IOException e
+        (println "ERROR: Could not connect to MongoDB."))
+      (catch java.lang.NullPointerException e
+        (println "ERROR: Could not authenticate with Mongo. See config: \n\t"
+                 (str (assoc config/storage :password "********")))))))
