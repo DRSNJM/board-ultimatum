fcfd7764b137f5059f0bf7fc7f891187b6b807ec
dalbz <david.albert09@gmail.com>
2012-10-02 20:14:19 -0400

    Fix indentation

--- src/board_ultimatum/views/recommend.clj
+++ src/board_ultimatum/views/recommend.clj
@@ -3,9 +3,10 @@
   (:use [noir.core :only [defpage]]))
 
 (defpage "/recommend" []
-         (common/layout
-           [:h1 "Want a game recommendation?"]
-           [:h2 "Fill in the inputs below with your preferences"]
-           [:form {:action "/games"} [:dl [:dt "Number of players"]
-                       [:dd [:input {:id "num-players"}]]]
-                  [:input {:type "submit" :value "Search"}]]))
+    (common/layout
+        [:h1 "Want a game recommendation?"]
+        [:h2 "Fill in the inputs below with your preferences"]
+        [:form {:action "/games"} 
+            [:dl [:dt "Number of players"]
+                 [:dd [:input {:id "num-players"}]]]
+            [:input {:type "submit" :value "Search"}]]))
