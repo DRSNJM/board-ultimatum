de8915fca72bd9d26a245c400c6762d5b3ac1a57
Ryan McGowan <ryan@ryanmcg.com>
2012-10-17 02:56:28 -0400

    Refactor expert.js.

--- resources/public/js/expert.js
+++ resources/public/js/expert.js
@@ -1,14 +1,14 @@
-window.onload = function() {
-	$('.btn-input').find('input[type=hidden]').val('off');
-}
+jQuery(document).ready(function ($) {
+  $('.btn-input').find('input[type=hidden]').val('off');
 
-// make game selections toggle hidden input
-$('.btn-input').live('click', function(event){
-	event.preventDefault();
-	var input = $(this).find('input[type=hidden]');
-	if ($(this).hasClass("active")) {
-		input.val('on');
-	} else {
-		input.val('off');
-	}
+  // make game selections toggle hidden input
+  $('.btn-input').on('click', function(event){
+    event.preventDefault();
+    var input = $(this).find('input[type=hidden]');
+    if ($(this).hasClass("active")) {
+      input.val('on');
+    } else {
+      input.val('off');
+    }
+  });
 });
