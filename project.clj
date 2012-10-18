534e582d82dfef9c426fe3200523a8f0ad4e54de
dalbz <david.albert09@gmail.com>
2012-10-18 18:54:38 -0400

    Set up clojure deps to run file as script

--- project.clj
+++ project.clj
@@ -6,4 +6,5 @@
                  [enclog "0.5.8-SNAPSHOT"]
                  [org.clojure/core.logic "0.7.5"]
                  [com.novemberain/monger "1.1.2"]]
+  :plugins [[lein-exec "0.2.1"]]
   :main board-ultimatum.server)
