76b3442fbb2adcc432f7b7525546139dd950cc08
Ryan McGowan <ryan@ryanmcg.com>
2012-10-29 18:15:03 -0400

    Start implementing expert auth and interface.
    Rename route from expert-select to expert/select. Add pre-route for
    expert/* routes that redirects requests from non-logged in users to the
    /expert route.
    
    Also, adds an expert route and some function stubs for logout and
    is-logged-in?.

--- src/board_ultimatum/views/expert.clj
+++ src/board_ultimatum/views/expert.clj
@@ -1,19 +1,47 @@
 (ns board-ultimatum.views.expert
-  (:require [board-ultimatum.views.common :as common])
-  (:use [noir.core :only [defpage defpartial]]))
+  "Namespace containing all expert views including login, logout, selection and
+  rating."
+  (:require [board-ultimatum.views.common :as common]
+            [board-ultimatum.flash :as flash]
+            [noir.response :as resp])
+  (:use [noir.core :only [defpage defpartial pre-route]]))
 
-; grid-cols must divide 12
+(defn expert-logged-in?
+  "Determine if there is an expert logged in right now."
+  [] false)
+
+(defn expert-logout "Logout the currently logged in expert."
+  [])
+
+(pre-route [:any "/expert/*"] {:as req}
+           (when-not (expert-logged-in?)
+             (flash/put! :warning "You must log in before accessing this
+                                  functionality.")
+             (resp/redirect "/expert")))
+
+;; The route from which an expert should start at. If they are not logged in
+;; they can here. If they are then they are redirected to select.
+(defpage "/expert" []
+  (common/layout
+    [:h1 "Welcome, Board Game Expert!"]
+    [:h2 "Please log in."]))
+
+;; Logout the currently logged in expert.
+(defpage "/expert/logout" []
+  (expert-logout)
+  (resp/redirect "/expert"))
+
+;; grid-cols must divide 12 for use with CSS grid system.
 (def grid-cols 3)
 (def grid-rows 3)
 (def grid-size (* grid-cols grid-rows))
 
-; Construct list of game titles and cover images
+;; Construct a list of game titles and cover images
 (def retrieved-games
   ; Currently produces dummy values, will eventually retrieve random games from database
   (for [x (range grid-cols)]
     (for [y (range grid-rows)]
       [(+ (* x grid-cols) y) "Game Title: Might Be a Bit Long" "http://lorempixel.com/150/100/"])))
-
 
 (defpartial game-thumb [[game-id game-name imgURL]]
   [:div.span4.game {:id (str "game-" game-id)}
@@ -26,7 +54,7 @@
   [:div.row-fluid
    (map game-thumb coll)])
 
-(defpage "/expert-select" []
+(defpage "/expert/select" []
   (common/with-javascripts (cons "/js/expert.js" common/*javascripts*)
     (common/layout
       [:h1 "Welcome, Board Game Expert!"]
@@ -37,5 +65,5 @@
         [:button.btn.btn-large.btn-primary.span6 "I know these games!"]
         [:a.btn.btn-large.span6 {:href "/expert-select"} "Try the next set"]]])))
 
-(defpage [:post "/expert-select"] {:as m}
+(defpage [:post "/expert/select"] {:as m}
   (str "parameters: " m))
