9b0a464dac6e59f31faa2eef5012d62346d4ceed
Ryan McGowan <ryan@ryanmcg.com>
2012-10-31 01:28:24 -0400

    Refactor flash namespace adding now!.

--- src/board_ultimatum/flash.clj
+++ src/board_ultimatum/flash.clj
@@ -4,19 +4,32 @@
   (:refer-clojure :exclude [get])
   (:require [noir.session :as sess]))
 
-(defn- a-put!
-  "Helper method to public put!. Wraps around flash-put!."
-  [t m] (sess/flash-put! :alert {:type t :message (apply str m)}))
+(defn- flash-now!
+  "Store a flash value that will persist for this request only."
+  [k v]
+  (swap! sess/*noir-flash* assoc-in [:incoming k] v))
 
-(defn put!
-  "A simple wrapper around noir's built-in flash put. Takes a variable number of
-  aguments. If the first argument is a keyword then it is assumed to be the type
-  of alert.  Otherwise the type is set to :info."
-  [flash-type & message]
-  (println message)
-  (if (keyword? flash-type)
-    (a-put! flash-type message)
-    (a-put! :info (cons flash-type message))))
+(defn- base-put!
+  "Most basic flash put the can either do an immediate or slow push using a
+  custom format on the :alert key."
+  [flasher t m] (flasher :alert {:type t :message (apply str m)}))
+
+(defn- put-partial
+  "A wrapper around base-put! that allows it to be used with a variable number
+  of arguments. The second argument is treated specially if it is a keyword."
+  [flasher]
+  (fn [flash-type & message]
+    (if (keyword? flash-type)
+      (base-put! flasher flash-type message)
+      (base-put! flasher :info (cons flash-type message)))))
+
+(def now!
+  "Does an immediate flash."
+  (put-partial flash-now!))
+
+(def put!
+  "Does a slow flash."
+  (put-partial sess/flash-put!))
 
 ;; Alias this namespace's get to noir's flash-get.
 (def get (partial sess/flash-get :alert))
