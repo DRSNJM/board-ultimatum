08c5cb93ba94521663dda1787fbcbe327f678fdd
dalbz <david.albert09@gmail.com>
2012-10-30 21:44:29 -0400

    Send dummy data through an untrained network

--- src/board_ultimatum/engine/neural.clj
+++ src/board_ultimatum/engine/neural.clj
@@ -1,9 +1,38 @@
 (ns board-ultimatum.engine.neural
+    (:require [taoensso.carmine :as car])
+    (:use clojure.pprint)
     (use [enclog nnets training]))
+
+; lein exec -p src/board_ultimatum/engine/neural.clj
 
 (def net
   (network  (neural-pattern :feed-forward)
     :activation :sigmoid
-    :input   200
+    :input   20
     :output  1
-    :hidden [200 100]))
+    :hidden [20 10]))
+
+(def training-set (data :basic-dataset [[0.0 0.0 0.0 0.0 0.0
+                                         0.0 0.0 0.0 0.0 0.0
+                                         0.0 0.0 0.0 0.0 0.0
+                                         0.0 0.0 0.0 0.0 0.0]]
+                                       [[1.0]]))
+
+(def input (data :basic-dataset [[0.0 0.0 0.0 0.0 0.0
+                                  0.0 0.0 0.0 0.0 0.0
+                                  0.0 0.0 0.0 0.0 0.0
+                                  0.0 0.0 0.0 0.0 0.0]
+                                 [1.0 1.0 1.0 1.0 1.0
+                                  1.0 1.0 1.0 1.0 1.0
+                                  1.0 1.0 1.0 1.0 1.0
+                                  1.0 1.0 1.0 1.0 1.0]]
+                                [[-1.0]
+                                 [-1.0]]))
+
+(def prop-train (trainer :resilient-prop :network net :training-set training-set)) 
+
+
+(doseq [pair input] 
+  (let [output (. net compute (. pair getInput ))] 
+  (println "Output = " (. output getData  0))))
+
