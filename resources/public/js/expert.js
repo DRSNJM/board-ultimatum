3900ae6e2796cee1597cc245d9cc295672bc6dd5
Ryan McGowan <ryan@ryanmcg.com>
2012-10-31 01:31:07 -0400

    Rewrite expert.js to work with new markup.
    Also does optimizations.

--- resources/public/js/expert.js
+++ resources/public/js/expert.js
@@ -2,13 +2,32 @@ jQuery(document).ready(function ($) {
   $('.btn-input').find('input[type=hidden]').val('off');
 
   // make game selections toggle hidden input
-  $('.btn-input').on('click', function(event){
+  var selectedCount = 0;
+  var $mainButton = $('#main-button');
+  var $mainButtonStrong = $mainButton.find('strong');
+  var origButtonText = $mainButtonStrong.html();
+  $('#expert-select').on('click', '.game', function (event) {
     event.preventDefault();
-    var input = $(this).find('input[type=hidden]');
-    if ($(this).hasClass("active")) {
-      input.val('on');
+    var $this = $(this);
+    var input = $this.find('input[type=hidden]');
+    var selectedClass = 'selected';
+    $this.toggleClass(selectedClass);
+    if ($this.hasClass(selectedClass)) {
+      input.val('true');
+      selectedCount++;
     } else {
-      input.val('off');
+      input.val('false');
+      selectedCount--;
+    }
+
+    if (selectedCount > 0) {
+      if (!$mainButton.hasClass('btn-primary')) {
+        $mainButtonStrong.html("I know these games!");
+        $mainButton.addClass('btn-primary');
+      }
+    } else {
+      $mainButton.removeClass('btn-primary');
+      $mainButtonStrong.html(origButtonText);
     }
   });
 });
