ffa0941d874379af72ea6c30475e65f4504305ad
Ryan McGowan <ryan@ryanmcg.com>
2012-10-30 20:18:53 -0400

    Create model.relationship.
    Handles expert defined relationships between games being
    stored and retrieved from the datastore.

---
+++ src/board_ultimatum/engine/model/relationship.clj
@@ -0,0 +1,28 @@
+(ns board-ultimatum.engine.model.relationship
+  "A namespace for maniuplating the experts as part of the datastore"
+  (:require [monger.core :as mg]
+            [monger.collection :as mc])
+  (:use [monger.operators]))
+
+(def ^:private coll
+  "The name of the collection on mongo containing relationships."
+  "relationships")
+
+(defn exists?
+  "An relationship between the two games exists in the database."
+  [games] (boolean (mc/find-one coll {:games {$all games}} [:_id])))
+
+(defn from-games
+  "Get an expert from the database by id."
+  ([games fields] (mc/find-one-as-map coll {:games {$all games}} fields))
+  ([games] (from-id id [])))
+
+(defn- relationship-to-object
+  "Converts the given relationship to an object which can be stored in mongo."
+  [relationship]
+  relationship)
+
+(defn add-many
+  "Add an expert with the given id to the datastore."
+  [game-relationships]
+  (mc/insert-batch coll (map relationship-to-object game-relationships)))
