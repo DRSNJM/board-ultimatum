09f7849bf8ebeb63366b6ab16b4fc345575c4d74
dalbz <david.albert09@gmail.com>
2012-10-31 21:34:52 -0400

    Store network output in mongo instead of redis - top 50 results only

--- src/board_ultimatum/engine/neural.clj
+++ src/board_ultimatum/engine/neural.clj
@@ -1,35 +1,27 @@
 (ns board-ultimatum.engine.neural
-    (:require [taoensso.carmine :as car])
-    (:use clojure.pprint)
-    (use [enclog nnets training]))
+  (:require [monger.core :as mg]
+            [monger.collection :as mc]
+            [board-ultimatum.engine.model :as model]
+            [board-ultimatum.engine.config :as config]
+            [taoensso.carmine :as car])
+  (:use clojure.pprint)
+  (use [enclog nnets training])
+  (:use [monger.operators]))
 
 ; lein exec -p src/board_ultimatum/engine/neural.clj
 
-(def net
-  (network  (neural-pattern :feed-forward)
-    :activation :sigmoid
-    :input   20
-    :output  1
-    :hidden [20 10]))
+(let [connection-info (if (nil? (:db-name config/storage))
+     (assoc config/storage :db-name "board_ultimatum") config/storage)]
 
-(def training-set (data :basic-dataset [[0.0 0.0 0.0 0.0 0.0
-                                         0.0 0.0 0.0 0.0 0.0
-                                         0.0 0.0 0.0 0.0 0.0
-                                         0.0 0.0 0.0 0.0 0.0]]
-                                       [[1.0]]))
+  (try
+    (model/connect connection-info)
+    (catch java.io.IOException e
+      (println "ERROR: Could not connect to MongoDB."))
+    (catch java.lang.NullPointerException e
+      (println "ERROR: Could not authenticate with Mongo. See config: \n\t"
+               (str (assoc connection-info :password "********"))))))
 
-(def dummy-input (data :basic-dataset [[0.0 0.0 0.0 0.0 0.0
-                                  0.0 0.0 0.0 0.0 0.0
-                                  0.0 0.0 0.0 0.0 0.0
-                                  0.0 0.0 0.0 0.0 0.0]
-                                 [1.0 1.0 1.0 1.0 1.0
-                                  1.0 1.0 1.0 1.0 1.0
-                                  1.0 1.0 1.0 1.0 1.0
-                                  1.0 1.0 1.0 1.0 1.0]]
-                                 [[-1.0]
-                                  [-1.0]]))
-
-(def prop-train (trainer :resilient-prop :network net :training-set training-set)) 
+;(mc/insert "network_output" { :first_name "John" :last_name "Lennon" })
 
 ;; establish redis connections
 ;; db 0 => list of vector values keyed on game id
@@ -48,6 +40,34 @@
 
 (pprint (wcar0 (car/ping)))
 
+;; network functions
+
+(def net
+  (network  (neural-pattern :feed-forward)
+    :activation :sigmoid
+    :input   20
+    :output  1
+    :hidden [20 10]))
+
+(def training-set (data :basic-dataset [[0.0 0.0 0.0 0.0 0.0
+                                         0.0 0.0 0.0 0.0 0.0
+                                         0.0 0.0 0.0 0.0 0.0
+                                         0.0 0.0 0.0 0.0 0.0]]
+                                       [[1.0]]))
+
+(def dummy-input (data :basic-dataset [[0.0 0.0 0.0 0.0 0.0
+                                       0.0 0.0 0.0 0.0 0.0
+                                       0.0 0.0 0.0 0.0 0.0
+                                       0.0 0.0 0.0 0.0 0.0]
+                                      [1.0 1.0 1.0 1.0 1.0
+                                       1.0 1.0 1.0 1.0 1.0
+                                       1.0 1.0 1.0 1.0 1.0
+                                       1.0 1.0 1.0 1.0 1.0]]
+                                      [[-1.0]
+                                       [-1.0]]))
+
+(def prop-train (trainer :resilient-prop :network net :training-set training-set)) 
+
 ;; for each id, join each id with every other id
 
 (def game-ids (wcar0 (car/keys "*")))
@@ -63,20 +83,25 @@
 (defn to-dataset [id-A id-B] (data :basic-dataset [(join-vector id-A id-B)]
                                 [[-1.0]]))
 
-(defn add-result [id-A id-B weight] (wcar2 
+(defn add-result-redis [id-A id-B weight] (wcar2 
   (car/zadd id-A weight id-B)))
+
+(defn add-result-mongo [id-A id-B weight]
+  (mc/insert "network_output" { :game_a id-A :game_b id-B :rating weight }))
 
 (defn output-pair [id-A id-B] (map 
   (fn [pair] (. (. net compute (. pair getInput)) getData 0))
   (to-dataset id-A id-B)))
 
-(doseq [id-A game-ids]
-  (doseq [id-B game-ids]
-    (add-result id-A id-B (nth (output-pair id-A id-B) 0))))
+;; iterate and add top 50 games to DB
+; (mc/insert "network_output"
 
-;(doseq [pair (to-dataset 2955 3685)] 
-;    (. (. net compute (. pair getInput)) getData 0))
+(doseq [id-A game-ids] 
+  (doseq [game-record (take 50 
+    (sort-by :rating >
+      (map 
+        (fn [id-B] 
+          { :rating (nth (output-pair id-A id-B) 0) :game_a id-A :game_b id-B })
+        game-ids)))]
+    (mc/insert "network_output" game-record)))
 
-;(pprint (map 
-;  (fn [pair] (. (. net compute (. pair getInput)) getData 0))
-;  (to-dataset 2955 3685)))
