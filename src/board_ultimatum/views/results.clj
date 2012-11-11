fcb91dc3e09b74ba2732f448ca009b42043ae509
Chris Powers <canasclone@gmail.com>
2012-11-11 17:10:48 -0500

    Display games as basic table of information

--- src/board_ultimatum/views/results.clj
+++ src/board_ultimatum/views/results.clj
@@ -29,7 +29,31 @@
    [:td (attr-display/format-score (:score game)) " points"]
    [:td.why (pp-factors game)]])
 
+(defpartial display-game2 [i game disp-recom disp-explanation]
+  [:div.well {:style "height:150px;"}
+    [:div {:style "width:200px;float:left;"}
+      [:img {:src (:thumbnail game) :style "margin: 0px auto;display: block;"}]]
+    [:table {:border "1" :style "float:left;margin-left:20px;width:75%;"}
+      [:tr {:style "line-height:50px;"}
+        [:td {:colspan "4" :style "font-size:34px;"}
+          (:name game)]]
+      [:tr {:style "height:80px;line-height:50px;"}
+        [:td {:style "width:55%;"}
+          "HEY"]
+        [:td {:style "width:15%;"}
+          (attr-display/game-length (:length game))]
+        [:td {:style "width:15%;"}
+          (attr-display/num-players (:min_players game) (:max_players game))]
+        [:td {:style "width:15%;"}
+          (:min_age game) "+"]]]])
+
 (defpartial build-results-list [games disp-recom disp-explanation]
+
+    (map display-game2
+      (iterate inc 1)
+      games
+      (iterate identity disp-recom)
+      (iterate identity disp-explanation))
 
     [:table.games.table.table-striped
     [:thead
