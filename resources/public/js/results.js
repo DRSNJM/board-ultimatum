fb3b0e8110055efd310a358c56c5578b4de790f1
Chris Powers <canasclone@gmail.com>
2012-11-12 15:05:58 -0500

    Add fadein/out on explanation, recommendation

--- resources/public/js/results.js
+++ resources/public/js/results.js
@@ -1,8 +1,12 @@
 jQuery(document).ready(function ($) {
-	$('.pop-trigger').popover({
-		html: true,
-		content: function() {
-			return $(this).siblings('.pop-content').html();
-		}
-	});
+  $('.pop-trigger').popover({
+    html: true,
+    content: function() {
+      return $(this).siblings('.pop-content').html();
+    }
+  });
+  $('.game').hover(
+    function() { $(this).find(".pop-trigger, .open-recom").fadeIn("fast"); },
+    function() { $(this).find(".pop-trigger, .open-recom").fadeOut("fast"); }
+  );
 });
