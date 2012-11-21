c623edf85392ae6b9cd3cf634d28c7f81640c1c9
Chris Powers <canasclone@gmail.com>
2012-11-21 00:57:41 -0500

    Add downward chevron, fix spacings

--- src/board_ultimatum/views/results.clj
+++ src/board_ultimatum/views/results.clj
@@ -27,18 +27,60 @@
       [:div {:style "float:left;"} "Total score"]
       [:div {:style "float:right;"} (attr-display/format-score (:score game))]]])
 
+(defpartial pp-factors-trigger [game]
+  [:div
+    [:div.pop-trigger {:style "position:absolute;right:5px;top:2px;float:left;display:none;"
+         :rel "popover" :data-placement "left" :data-trigger "hover"
+         :data-title "How did this game match up to your preferences?"}
+      [:i.icon-question-sign {:style "margin-left:10px;"}]]
+    [:div.pop-content {:style "display:none;"} (pp-factors game)]])
+
+(defpartial mechanics-categories [game]
+  (map #(identity [:span {:style "width:50%;float:left;margin-bottom:2px;"} "&#149; " %])
+    (concat (model/mechanics game)
+      (model/categories game))))
+
+(defpartial length [game]
+  (let [length-disp
+    (string/split (attr-display/game-length (:length game)) #"\s+")]
+    [:div {:style "text-align:center;border-left:1px solid black;"}
+      [:div {:style "font-size:30px;"} (first length-disp)]
+      [:div (second length-disp)]]))
+
+(defpartial num-players [game]
+  (let [num-pl-disp
+    (string/split (attr-display/num-players (:min_players game) (:max_players game)) #"\s+")]
+    [:div {:style "text-align:center;border-left:1px solid black;"}
+      [:div {:style "font-size:30px;"} (first num-pl-disp)]
+      [:div (second num-pl-disp)]]))
+
+(defpartial min-age [game]
+  [:div {:style "text-align:center;border-left:1px solid black;"}
+    [:div {:style "font-size:30px;"} (:min_age game) "+"]
+    [:div "years old"]])
+
+(defpartial weight [game]
+  [:div {:style "text-align:center;border-left:1px solid black;"}
+    (let [text (string/split (game-weight-text (:weight_average game)) #"\s+")]
+      (if
+        (< 1 (.size text))
+        [:b (map #(identity [:div {:style "font-size:16px;"} %]) text)]
+        (identity [:div {:style "font-size:30px;"} (first text)])))
+    [:div "weight"]])
+
+(defpartial recom-trigger []
+  [:div {:style "position:absolute;left:50%;bottom:0px;"}
+    [:div.open-recom {:style "display:none;position:relative;left:-50%;height:30px;width:40px;"}
+      [:i.icon-chevron-down]]])
+
 (defpartial display-game [i game disp-recom disp-explanation rating]
   [:div.well.game {:style "height:150px;position:relative;"}
     (if (and disp-explanation (not= (.size (:factors game)) 0))
-      [:div
-        [:div.pop-trigger {:style "position:absolute;right:5px;top:2px;float:left;display:none;"
-             :rel "popover" :data-placement "left" :data-trigger "hover"
-             :data-title "How did this game match up to your preferences?"}
-          [:i.icon-question-sign {:style "margin-left:10px;"}]]
-        [:div.pop-content {:style "display:none;"} (pp-factors game)]])
-    [:div {:style "width:200px;float:left;"}
+      (pp-factors-trigger game))
+    (if disp-recom (recom-trigger))
+    [:div {:style "width:20%;float:left;"}
       [:img {:src (:thumbnail game) :style "margin: 0px auto;display: block;"}]]
-    [:table {:style "float:left;margin-left:20px;width:75%;line-height:normal;"}
+    [:table {:style "float:right;margin-left:20px;width:78%;line-height:normal;"}
       [:tr {:style "height:50px;border-bottom:1px solid black;"}
         [:td {:colspan "5"}
           [:div {:style "font-size:34px;float:left;"}
@@ -47,43 +89,19 @@
           [:div {:style "float:right;"} 
             (link-to
               (str "http://boardgamegeek.com/boardgame/" (:bgg_id game) "/")
-              "BGG Rank: " (:rank game) " "
-              [:i {:class "icon-share"}])]]]
+              "BGG Rank: " (:rank game)
+              [:i.icon-share {:style "margin-left:4px;"}])]]]
       [:tr {:style "height:80px;"}
         [:td {:style "width:50%;"}
-          (map #(identity [:span {:style "width:50%;float:left;margin-bottom:2px;"} "&#149; " %])
-            (concat (model/mechanics game)
-              (model/categories game)))]
-        [:td {:style "width:12%;"}
-          (let [length-disp
-            (string/split
-              (attr-display/game-length (:length game))
-              #"\s+")]
-            [:div {:style "text-align:center;border-left:1px solid black;"}
-              [:div {:style "font-size:30px;"} (first length-disp)]
-              [:div (second length-disp)]])]
-        [:td {:style "width:12%;"}
-          (let [num-pl-disp
-            (string/split 
-              (attr-display/num-players (:min_players game) (:max_players game))
-              #"\s+")]
-            [:div {:style "text-align:center;border-left:1px solid black;"}
-              [:div {:style "font-size:30px;"} (first num-pl-disp)]
-              [:div (second num-pl-disp)]])]
-        [:td {:style "width:12%;"}
-          [:div {:style "text-align:center;border-left:1px solid black;"}
-            [:div {:style "font-size:30px;"} (:min_age game) "+"]
-            [:div "years old"]]]
-        [:td {:style "width:14%;"}
-          [:div {:style "text-align:center;border-left:1px solid black;"}
-            (let [text (string/split (game-weight-text (:weight_average game)) #"\s+")]
-              (if
-                (< 1 (.size text))
-                [:b (map #(identity [:div {:style "font-size:16px;"} %]) text)]
-                (identity [:div {:style "font-size:30px;"} (first text)])))
-            [:div "weight"]]]]]
-    [:div.open-recom {:style "height:150px;width:20px;float:right;display:none;"}
-      [:i.icon-chevron-right {:style "position:absolute;top:45%;"}]]])
+          (mechanics-categories game)]
+        [:td {:style "width:11%;"}
+          (length game)]
+        [:td {:style "width:11%;"}
+          (num-players game)]
+        [:td {:style "width:11%;"}
+          (min-age game)]
+        [:td {:style "width:17%;"}
+          (weight game)]]]])
 
 ;; Send true for disp-recom, disp-explanation if you wish to display recommendations
 ;; and explanations on games. Ratings should be nil if no ratings are to be displayed.
