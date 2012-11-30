9ddf493e38a7c3cbeefcff4cc3d4f8c206d3c4a1
Alex Burkhart <saterus@gmail.com>
2012-11-29 19:24:37 -0500

    Redirect to recommend from index

--- src/board_ultimatum/views/index.clj
+++ src/board_ultimatum/views/index.clj
@@ -1,16 +1,8 @@
 (ns board-ultimatum.views.index
   (:require [board-ultimatum.views.common :as common])
-  (:use [noir.core :only [defpage]]
+  (:use [noir.core :only [defpage pre-route]]
+        [noir.response :only [redirect]]
         [hiccup.element]))
 
-(defpage "/" []
-         (common/layout
-           [:h1 "Welcome to board-ultimatum"]
-           [:p "The board-ultimatum is a board game recommendation system" 
-               [:br] 
-            "This system is implemented in Clojure"]
-
-           [:h3 "Pages"]
-           [:p (link-to "/recommend" "Recommend V1")]
-           [:p (link-to "/tags" "Tag Administration")]
-           [:p (link-to "/expert" "Expert Interface")]))
+(pre-route "/" {}
+           (redirect "/recommend"))
